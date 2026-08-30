17b5261bb5ac941da477822a88c4743e37d291e8
polo0700 <jorrgeee28@hotmail.com>
2026-08-30 13:50:32 -0600

    Agrega tabla lista de deseos

---
+++ database/migrations/2026_08_30_193149_create_lista_deseos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lista_deseos', function (Blueprint $table) {
+            $table->id('id_deseo');
+            $table->foreignId('id_usuario')->constrained('usuarios');
+            $table->foreignId('id_alimento')->constrained('alimentos');
+            $table->timestamp('fecha_agregado')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lista_deseos');
+    }
+};
